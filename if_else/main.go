f2cb73c20565044ac2ac808b6ae1ef05c207b7e9
divy404 <divyanshj360@gmail.com>
2025-07-28 21:18:16 +0530

    if-else

---
+++ if_else/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 10
+	if x == 0 {
+		fmt.Println("no worries brruh")
+	} else {
+		fmt.Println("naah dude")
+	}
+
+	p := 14
+	if p == 11 {
+		fmt.Println("all good")
+	} else if p < 15{
+		fmt.Println("this is also good")
+	} else {
+		fmt.Println("nothing dudeee")
+	}
+}
