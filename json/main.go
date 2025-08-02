4f5d29d7878882e28ba562763c9cd9a191895d24
divy404 <divyanshj360@gmail.com>
2025-08-03 01:26:54 +0530

    more about json

--- json/main.go
+++ json/main.go
@@ -1,5 +1,36 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Person struct {
+	Name string `json:"name"`
+	Age int `json:"age"`
+	IsAdult bool `json:"is_adult"`
+}
+
 func main() {
-	
+	fmt.Println("we are learning json")
+	person := Person{Name:"John", Age: 34, IsAdult: true}
+	// fmt.Println("person Data is:", person)
+
+	//  convert person into JSON Encoding (Marshalling)
+	jsonData, err := json.Marshal(person)
+	if err != nil {
+		fmt.Println("Error Marshalling", err)
+		return
+	}
+	fmt.Println("person Data is: ", string(jsonData))
+
+	// Decoding (Unmarshalling)
+	var personData Person
+	err = json.Unmarshal(jsonData,&personData)
+	if err != nil {
+		fmt.Println("error unmarshalling", err)
+		return
+	}
+	fmt.Println("person Data is:", personData)
+
 }
